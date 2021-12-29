eebc8b2b63ac08ad24d6cbd3cae328ed73b20d96
Dawnie Patola <danielpatola0@gmail.com>
2021-12-29 16:13:09 +0100

    javascript tabele galeria

---
+++ index.js
@@ -0,0 +1,32 @@
+
+var zm1 = "jakikolwiek";
+var zm2 = 2;
+var zm3 = "2";
+var wynik = zm2 + " sztuki";
+
+function otwieracz() {
+    alert("otwarto")
+}
+function zaawansowanyOtwieracz(x) {
+    if (x > 5) {
+        alert("większe od 5")
+    } else { alert("mniejsze niż 5") }
+}
+function zo(x) {
+    x = document.getElementById("text").value
+
+
+
+    if (x >= 5 && x == true) {
+        alert("cokolwiek")
+    }
+    else {
+        if (!x) {
+            alert("nie podano wartości")
+        }
+        else {
+            alert("niecokolwiek")
+        }
+    }
+
+}
